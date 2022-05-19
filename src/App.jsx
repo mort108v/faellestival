2359bd3ca0a8083fbcceac2bf9c6bcca6e52b6f5
ailin torre val <aili0057@stud.kea.dk>
2022-05-19 12:51:43 +0200

    ailin-first fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,55 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { BandsContext } from "./Contexts/BandsContext.js";
+import { LoginContext } from "./Contexts/LoginContext.js";
+import { TicketsContext } from "./Contexts/TicketsContext.js";
 
-export const envData = {
-  availableSpots: process.env.FAELLESTIVAL_AVAILABLE_SPOTS,
-  bands: process.env.FAELLESTIVAL_BANDS,
-  events: process.env.FAELLESTIVAL_EVENTS,
-  fullfillReservation: process.env.FAELLESTIVAL_FULLFILL_RESERVATION,
-  schedule: process.env.FAELLESTIVAL_SCHEDULE,
-  reserveSpot: process.env.FAELLESTIVAL_RESERVE_SPOT,
-  settings: process.env.FAELLESTIVAL_SETTINGS,
-};
+// export const envData = {
+//   availableSpots: process.env.FAELLESTIVAL_AVAILABLE_SPOTS,
+//   bands: process.env.FAELLESTIVAL_BANDS,
+//   events: process.env.FAELLESTIVAL_EVENTS,
+//   fullfillReservation: process.env.FAELLESTIVAL_FULLFILL_RESERVATION,
+//   schedule: process.env.FAELLESTIVAL_SCHEDULE,
+//   reserveSpot: process.env.FAELLESTIVAL_RESERVE_SPOT,
+//   settings: process.env.FAELLESTIVAL_SETTINGS,
+// };
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [bandsData, setBandsData] = useState([]);
+  const [isLogin, setIsLogin] = useState(false);
+  const [scheduleData, setScheduleData] = useState([]);
 
+  useEffect(() => {
+    fetch("https://a3m-festival.herokuapp.com/bands")
+      .then((res) => res.json())
+      .then((data) => {
+        setBandsData(data);
+        console.log(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://a3m-festival.herokuapp.com/schedule")
+      .then((res) => res.json())
+      .then((sdata) => {
+        setScheduleData(sdata);
+        console.log(sdata);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <BandsContext.Provider value={bandsData}>
+        <TicketsContext.Provider value={scheduleData}>
+          <LoginContext.Provider value={isLogin}>
+            {isLogin ? <FestApp /> : <RegApp />}
+          </LoginContext.Provider>
+        </TicketsContext.Provider>
+      </BandsContext.Provider>
+    </>
   );
+}
+
+function RegApp() {
+  return <h1>Faellestival</h1>;
 }
 
 export default App;
