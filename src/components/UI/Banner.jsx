68970492b59b7a80cfcc3d92ba8ed7a71b53abc7
ailin torre val <aili0057@stud.kea.dk>
2022-05-24 15:09:41 +0200

    ailin-merge to main

--- src/components/UI/Banner.jsx
+++ src/components/UI/Banner.jsx
@@ -2,29 +2,35 @@ import React, {useContext} from 'react';
 import { BandsContext } from "../../Contexts/BandsContext.js";
 import { ScheduleContext } from "../../Contexts/ScheduleContext.js";
 
-
 function Banner(props) {
-    const bandsData = useContext(BandsContext);
-    const stagesData = useContext(ScheduleContext);
-    
-    const myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd']
-    let myArrayWithNoDuplicates = myArray.reduce(function (previousValue, currentValue) {
-      if (previousValue.indexOf(currentValue) === -1) {
-        previousValue.push(currentValue)
-      }
-      return previousValue
-    }, [])
-    
-    console.log(myArrayWithNoDuplicates)
-  return (
-    <>
-{props.content && <p>{props.content}</p>}
-{props.type == "stages" && <p>{stagesData.length} Stages </p> }
-{props.type == "bandsQty" && <p>{bandsData.length} Bands </p> }
-{props.type == "genre" && <p>{bandsData.length} genres </p> }
-
-    </>
-  )
+    return (
+        <p>
+            {props.children}
+        </p>
+    )
 }
+
+// function Banner(props) {
+//     const bandsData = useContext(BandsContext);
+//     const stagesData = useContext(ScheduleContext);
+    
+//     // const myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd']
+//     // let myArrayWithNoDuplicates = myArray.reduce(function (previousValue, currentValue) {
+//     //   if (previousValue.indexOf(currentValue) === -1) {
+//     //     previousValue.push(currentValue)
+//     //   }
+//     //   return previousValue
+//     // }, [])
+    
+//     // console.log(myArrayWithNoDuplicates)
+//   return (
+//     <>
+// {props.content && <p>{props.content}</p>}
+// {props.type == "bandsQty" && <p>{bandsData.length} Bands </p> }
+// {props.type == "genre" && <p>{bandsData.length} genres </p> }
+
+//     </>
+//   )
+// }
 
 export default Banner
