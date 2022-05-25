90b38f94703a91d88bac03dd3bcd2aaa69244d48
ailin torre val <aili0057@stud.kea.dk>
2022-05-25 15:43:45 +0200

    iage working, needs styling

--- src/components/UI/ArtistCard.jsx
+++ src/components/UI/ArtistCard.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-let imgPath;
-
 
 function ArtistCard(props) {
+  let imgPath;
   let img = props.img;
   img.includes("http")
-    ? (imgPath = "url(" + props.img + ")")
-    : (imgPath = "url(https://a3m-festival.herokuapp.com/logos/" + img);
+    ? (imgPath = props.img)
+    : (imgPath = "https://a3m-festival.herokuapp.com/logos/" + img);
   console.log(imgPath);
 
   //         let root = document.documentElement;
   //   root.style.setProperty('--img', imgPath);
 
   return (
-    <button className={props.className} onClick={props.action}>
-      <h2> {props.content}</h2>
-    </button>
+    <>
+      <button className={props.className} onClick={props.action}>
+      {/* <img src={imgPath} alt={props.content} /> */}
+        <h2> {props.content}</h2>
+      </button>
+    </>
   );
 }
 
