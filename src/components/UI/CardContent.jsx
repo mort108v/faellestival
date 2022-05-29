7fc6a3e4f45a3c0b2f58edb33d0857118257242d
Maja Berendsen <maja498h@stud.kea.dk>
2022-05-29 16:34:20 +0200

    stylling for Landing page

--- src/components/UI/CardContent.jsx
+++ src/components/UI/CardContent.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function CardContent(props) {
   return (
-<div className={props.className}>
+<div className="cardInfo">
 <p>{props.content}</p>
 {props.content2 && <p className="cardContent2" >{props.content2}</p>}
-{props.price && <p>From {props.price}kr.</p>}
+{props.price && <p className='cardPrice'>From {props.price}kr.</p>}
 </div>
   )
 }
