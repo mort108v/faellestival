46454dd45cee33551f3966d8729cde09541ba184
ailin torre val <aili0057@stud.kea.dk>
2022-05-30 15:38:23 +0200

    starting addOn section

---
+++ src/components/sections/AddOnsSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AddOnsSection() {
+  return (
+    <section className="addOnSection">
+        <SectionTitle className="formTitle" content="Get the Full Faellestival Experience" />
+        <div className="cardGrid">
+            
+        </div>
+    </section>
+  )
+}
+
+export default AddOnsSection
