adb44eceb114baabe975561a39903538d9bb105d
Morten Six <mort108v@stud.kea.dk>
2022-05-31 00:41:56 +0200

    Added resolve path to root and index

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import path from "path";
 
-// const { resolve } = require('path')
-// const rootSrc = resolve(__dirname, "../src");
-// https://vitejs.dev/config/
+const { resolve } = require('path')
+    // const rootSrc = resolve(__dirname, "../src");
+    // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // build: {
-    //     rollupOptions: {
-    //         input: {
-    //             main: resolve(__dirname, 'index.html'),
-    //         }
-    //     }
-    // },
-    // root: './'
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                root: resolve(__dirname, './'),
+
+            }
+        }
+    },
     // css: {
     //     preprocessorOptions: {
     //         scss: { additionalData: `@import "${rootSrc}/scss/_config";` },
