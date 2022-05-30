1c00c3ccbd40d10f6c67666fe0d8742ea5fcc66e
ailin torre val <aili0057@stud.kea.dk>
2022-05-30 13:53:26 +0200

    some fixes

--- src/components/LineupPage.jsx
+++ src/components/LineupPage.jsx
@@ -12,19 +12,20 @@ function LineupPage() {
   const dayShort = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
   return (
+    <>
     <div className="lineupGrid">
-
       {dayShort.map((day, index) => (
         <div className="lineupCard">
         <CardTitle className="lineupTitle" head={week[index]} subHead={date[index]} />
         <p className="lineupCardContent">
-          {scheduleData.Jotunheim[day].map((day) => <Artist content={day.act} />)}
-          {scheduleData.Vanaheim[day].map((day) => <Artist content={day.act} />)}
-          {scheduleData.Midgard[day].map((day) => <Artist content={day.act} />)}
+          {scheduleData.Jotunheim[day].map((day) => (day.act !== "break" && <Artist content={day.act} />))}
+          {scheduleData.Vanaheim[day].map((day) => (day.act !== "break" && <Artist content={day.act} />))}
+          {scheduleData.Midgard[day].map((day) => (day.act !== "break" && <Artist content={day.act} />))}
           </p>
         </div>))}
 
     </div>
+    </>
   );
 }
 
